b44c49aba4f383c542a1ae2b14f353ddde77e91a
Isielu Ruphina <106762380+Isielu@users.noreply.github.com>
2022-07-06 19:26:39 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,43 @@
+#include "main.h"
+<<<<<<< HEAD
+
+/**
+* main - prints Holberton followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+=======
+/**main - prints putchar
+*Return: always 0
+*/
+>>>>>>> 992b8e1837fa3c17303b57456df32711e1cc2531
+
+int main(void)
+{
+<<<<<<< HEAD
+
+=======
+>>>>>>> 992b8e1837fa3c17303b57456df32711e1cc2531
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+<<<<<<< HEAD
+	_putchar('r');
+  _putchar('\n');
+
+	return (0);
+
+=======
+_putchar('r');
+_putchar('\n');
+
+	return (0);
+>>>>>>> 992b8e1837fa3c17303b57456df32711e1cc2531
+}
+
+
 
